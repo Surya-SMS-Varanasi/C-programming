e85b523fc53af6b243b78dcb2dd3232dc82d359c
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-03-11 17:47:40 +0530

    Add basics.c with display function implementation

---
+++ Intermediate_C/Function/basics.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void display();//Function Declaration.
+
+int main(){
+
+  display(); // Function Calling.
+  return 0;
+}
+void display(){ //Function Definition.
+  printf("Hello from dispaly()");
+}
