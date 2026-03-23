a6ac7783d9204b4bc0ffe51833b245f0d5394b17
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-03-23 23:51:19 +0530

    Add LinkedList_Display.c to display linked list
    Implement a linked list display function to print node data.

---
+++ DSA-in-C/Linked-List/Singly-linked-List/LinkedList_Display.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Linked{
+	int data;
+	struct Linked *nextAdd;
+};
+
+void display(struct Linked *ptr){
+	while(ptr != NULL){
+		printf("%d ",ptr->data);
+		ptr = ptr->nextAdd;
+	}
+}
+
+void main(){
+	struct Linked *first, *second, *third, *fourth;
+
+	first = (struct Linked *) malloc(sizeof(struct Linked));
+	second = (struct Linked *) malloc(sizeof(struct Linked));
+	third = (struct Linked *) malloc(sizeof(struct Linked));
+	fourth = (struct Linked *) malloc(sizeof(struct Linked));
+
+	first->data = 90;
+	first->nextAdd = second;
+
+	second->data = 40;
+	second->nextAdd = third;
+
+	third->data = 80;
+	third->nextAdd = fourth;
+
+	fourth->data = 20;
+	fourth->nextAdd = NULL;
+
+	display(first);
+}
